ceec5811021607371cf45e4496f51c6dcf767fb9
azzzfath <AzzamFathurrahman23@gmail.com>
2025-12-20 00:25:51 +0700

    schedule/match

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:garudaspot_flutter/schedule/macth_list.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
 
@@ -29,6 +30,7 @@ class MyApp extends StatelessWidget {
           '/register': (_) => const RegisterPage(),
           '/news': (_) => const NewsPage(),
           '/merch': (_) => const MerchPage(),
+          '/schedule': (_) => const MatchListPage(),
         },
       ),
     );
